67d8a151f162d301fd8708a4a08b8280802d90ad
Xiaoyi Zhang <iamxyz@bu.edu>
2016-09-08 19:14:16 -0400

    new commit

---
+++ class/script-plot.R
@@ -0,0 +1,49 @@
+# press Ctrl + Enter to run every line.
+
+
+# 1 download a package for plotting (ggplot2)
+# make sure that your computer is connected to the Internet.
+install.packages("ggplot2")
+
+
+# 2 Attach the ggplot2 package to your workspace. 
+# This is what you must do in order to use functions in this package.
+library(ggplot2)
+
+
+# 3 Read the document of ggplot function in ggplot2.
+?ggplot
+
+
+# 4 Make up some data and the draw a plot
+df <- data.frame(gp = factor(rep(letters[1:3], each = 10)),
+                 y = rnorm(30))
+# Compute sample mean and standard deviation in each group
+ds <- plyr::ddply(df, "gp", plyr::summarise, mean = mean(y), sd = sd(y))
+
+# Declare the data frame and common aesthetics.
+# The summary data frame ds is used to plot
+# larger red points in a second geom_point() layer.
+# If the data = argument is not specified, it uses the
+# declared data frame from ggplot(); ditto for the aesthetics.
+ggplot(df, aes(x = gp, y = y)) +
+  geom_point() +
+  geom_point(data = ds, aes(y = mean),
+             colour = 'red', size = 3)
+# Same plot as above, declaring only the data frame in ggplot().
+# Note how the x and y aesthetics must now be declared in
+# each geom_point() layer.
+ggplot(df) +
+  geom_point(aes(x = gp, y = y)) +
+  geom_point(data = ds, aes(x = gp, y = mean),
+             colour = 'red', size = 3)
+# Set up a skeleton ggplot object and add layers:
+ggplot() +
+  geom_point(data = df, aes(x = gp, y = y)) +
+  geom_point(data = ds, aes(x = gp, y = mean),
+             colour = 'red', size = 3) +
+  geom_errorbar(data = ds, aes(x = gp, y = mean,
+                               ymin = mean - sd, ymax = mean + sd),
+                colour = 'red', width = 0.4)
+
+
